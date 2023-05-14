0b1212b12cd6912b62059cbccd033a0f03593333
saurabhbgp9693 <saurabhbgp9693@gmail.com>
2023-05-14 17:09:48 +0530

    backgroung and style changes

--- main.go
+++ main.go
@@ -10,9 +10,6 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	// router.RouterHandler()
-	// Start the server
-	// http.HandleFunc("/", controller.Handlers)
 	router.RouterHandler()
 
 	log.Fatal(http.ListenAndServe(":8082", nil))
